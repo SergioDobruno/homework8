e2adc4d7823cbf8270b1c4dc22b98f073482eda3
Sergio Dobrunoclear <dobryakofff.s@yandex.ruclear>
2023-09-08 22:12:00 +0300

    Задача 54 успешно решена

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+int[,] table = new int[3, 4];
+FillMatrixRandom(table);
+PrintMatrix(table);
+SortToLower(table);
+Console.WriteLine();
+PrintMatrix(table);
+
+void FillMatrixRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            matrix[i, l] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void SortToLower(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write($"{matrix[i, l]} ");
+        }
+        Console.WriteLine();
+    }
+}
